2fd162c3ad2e107bd3590d596c77c70076484bdc
barry.shang <barry.shang@gmail.com>
2016-01-21 08:23:48 -0800

    Initial commit.

--- Ex1_Connection/Program.cs
+++ Ex1_Connection/Program.cs
@@ -17,8 +17,8 @@ namespace PI_AF_SDK_For_Beginners_TechCon2016
             // Uncomment the lines below in order to test
             // AFDatabase db = AFConnection.ImplicitConnect(Constants.AFSERVERNAME, "databaseName");
             // AFDatabase db = AFConnection.ExplicitConnect("serverName", "databaseName"); 
+            // if (db != null) Console.WriteLine("Connected to database: {0}", db.Name);
 
-            if (db != null) Console.WriteLine("Connected to database: {0}", db.Name);
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
